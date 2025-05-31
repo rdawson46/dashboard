0f505b1b2c0f14fd0978cf99f8c702e222129797
rdawson46 <dawson4623@gmail.com>
2025-05-31 16:42:50 -0400

    starter for vue ui

---
+++ ui/src/components/Auth.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+    <p>{{msg}}</p>
+
+  <div class="card">
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+
+</template>
+
+<style scoped>
+</style>

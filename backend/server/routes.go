fe6b51571212aee6be6be59af29bd673f907d468
rdawson46 <dawson4623@gmail.com>
2025-06-01 09:47:30 -0400

    file for adding routes to the server

---
+++ backend/server/routes.go
@@ -0,0 +1,17 @@
+package server
+
+import (
+    "net/http"
+)
+
+// ========== ROUTING FUNCTIONS ==========
+// =======================================
+
+
+// ========== HANDLER FUNCTIONS ==========
+
+func addRoutes(h *http.ServeMux) {
+    // h.HandleFunc
+}
+
+// =======================================

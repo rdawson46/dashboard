447bdee62fa42e91ffcfb1788ab01bdd492a691e
rdawson46 <dawson4623@gmail.com>
2025-08-05 00:10:41 -0400

    small change I noticed

--- backend/api/ollama.go
+++ backend/api/ollama.go
@@ -30,7 +30,7 @@ func getWebSearchTool() api.Tool {
 			Parameters: struct{Type string "json:\"type\""; Defs any "json:\"$defs,omitempty\""; Items any "json:\"items,omitempty\""; Required []string "json:\"required\""; Properties map[string]struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""} "json:\"properties\""}{
 				Type: "object",
 				Properties: map[string]struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""}{
-					"query": struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""}{
+					"query": {
 						Type: []string{"string"},
 						Description: "query to help answer the user's question",
 					},
@@ -50,7 +50,7 @@ func getCodeExecution() api.Tool {
 			Parameters: struct{Type string "json:\"type\""; Defs any "json:\"$defs,omitempty\""; Items any "json:\"items,omitempty\""; Required []string "json:\"required\""; Properties map[string]struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""} "json:\"properties\""}{
 				Type: "object",
 				Properties: map[string]struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""}{
-					"code": struct{Type api.PropertyType "json:\"type\""; Items any "json:\"items,omitempty\""; Description string "json:\"description\""; Enum []any "json:\"enum,omitempty\""}{
+					"code": {
 						Type: []string{"string"},
 						Description: "code that you produced",
 					},

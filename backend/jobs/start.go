88cf7cfef391a9192312f0ce5d0a93f5c18ebb13
rdawson46 <dawson4623@gmail.com>
2025-10-17 22:54:57 -0400

    added start function for job pipeline

--- backend/jobs/start.go
+++ backend/jobs/start.go
@@ -1,5 +1,13 @@
 package jobs
 
-func StartJobCheck() {
+import (
+	"context"
+	"database/sql"
+)
 
+// TODO: don't use background context, use one with cancel
+func StartJobCheck(ctx context.Context, db *sql.DB, workers int) {
+    j := make(chan Job, 10)
+    go StartWorkerPool(ctx, j, workers)
+    StartScheduler(ctx, db, j)
 }
